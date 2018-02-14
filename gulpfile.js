a678a1db10e2cb1d6ab1b1d38e58de103d25eee5
arianrhod <pataki.annamaria@hotmail.com>
2018-02-14 13:44:45 +0100

    'Cmderrrrben'

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,16 @@ gulp.task('html', function(){
 	console.log("Somthing useful happening with our html file.");
 });
 
+gulp.task('styles', function(){
+	console.log("Imagine something cool happening with our css file.");
+});
+
 gulp.task('watch', function(){
 
 	watch('index.html', function(){
 		gulp.start('html');
+	});
+	watch('./app/styles/**/*.css', function(){
+		gulp.start('styles');
 	});
 });
